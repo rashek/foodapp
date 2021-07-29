1df032e2665d70cc6beb73c7057c6ab29230c0a7
rashek <rashekhumayun@gmail.com>
2021-07-30 00:20:25 +0600

    dummy data complete

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/foundation.dart';
 
-enum Complexity { Simple, Challenging, Hard }
+enum EatingHabit { Carnivour, Harbivour, Omnivour }
 
-enum Affordability { Affordable, Pricey, Luxurious }
-
-class Meal {
+class Animal {
   final String id;
-
   final String title;
   final String imageUrl;
-  final String eatingHabit;
+  final EatingHabit eatingHabit;
   final String description;
   final bool isBird;
+  final bool isMammal;
+  final bool isReptile;
+  final bool isFish;
   final bool isNative;
   final bool isHunter;
-  const Meal({
+  final bool isInsect;
+  const Animal({
     @required this.id,
     @required this.title,
     @required this.imageUrl,
     @required this.eatingHabit,
     @required this.description,
     @required this.isBird,
+    @required this.isMammal,
+    @required this.isReptile,
+    @required this.isFish,
     @required this.isHunter,
     @required this.isNative,
+    @required this.isInsect,
   });
 }
